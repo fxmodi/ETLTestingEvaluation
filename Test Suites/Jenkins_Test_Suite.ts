f3fe2f625a83c0836dc8e4084c591e83b40dbcc7
Fremil Modi <Fremil.Modi@expresspros.com>
2024-10-25 14:26:53 +0530

    2nd commit

---
+++ Test Suites/Jenkins_Test_Suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Jenkins_Test_Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>48cd5f5d-008e-4f76-92eb-2822e49abf3b</testSuiteGuid>
+</TestSuiteEntity>
